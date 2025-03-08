18ed22a0307c35eb61fc5385061155014769480c
marcinkirkley <hi@kirkley.co>
2025-03-08 17:59:40 -0500

    Your commit message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
+// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
+export default defineConfig({
+  integrations: [mdx()],
+});
 // https://astro.build/config
-export default defineConfig({});
